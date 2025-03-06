c5cf29e6aa8046fd17b4275bf7870f227762e5a4
Jonathan Dabre <dabrejonathan.manuel@wissen.com>
2025-03-06 15:38:27 +0530

    Add structural directive component with functionality and update app component

--- src/app/structural-directive/structural-directive.component.ts
+++ src/app/structural-directive/structural-directive.component.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-structural-directive',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './structural-directive.component.html',
   styleUrl: './structural-directive.component.css'
 })
 export class StructuralDirectiveComponent {
+  flag:boolean = true;
+  fruits :string[] = ['Apple', 'Mango', 'Grapes']
+  count:number = 0;
 
+  toggle():void{
+    this.flag = !this.flag;
+  }
+  addFruit(f:string){
+    this.fruits.push(f);
+  }
+  incrCount():void{
+    this.count = (this.count + 1)%5;
+  }
 }
